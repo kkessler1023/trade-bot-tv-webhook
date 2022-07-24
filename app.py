348bb5d2b3c9df72bc1b44af3a60e4fa390abc87
kkessler1023 <kessler.kasey@gmail.com>
2022-07-24 18:25:14 -0500

    Main app
    This file holds the app, routes, and database table class

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
